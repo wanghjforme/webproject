c6ea1a580334d6a619876cf32c02c9b99bafde30
wanghjforme <1219368500@qq.com>
2015-12-04 18:24:47 +0800

    20151123

--- jspro/js/func.js
+++ jspro/js/func.js
@@ -1,50 +1,113 @@
 /**
- * Created by xf on 2015/11/23.
+ * Created by whj on 2015/11/23.
  */
-function testArray(){
+
+//数组里可以存储不同类型的值
+function testArrayValue(){
     var arr = new Array(3);
-    arr[0] = function (x) {
-        return x*x;
-    };
+    arr[0] = function (x) {return x*x;};
     arr[1] = 20;
-    arr[2] = arr[0](arr[1]);
-    document.write(arr[2]+"\n\r");
+    arr[2] = "abc";
+    arr[3] = arr[0](arr[1]);
+    document.write(arr[3]+"<br>");
+}
+//测试工具函数
+function testArray(){
     var m = copyProperties({x:1,y:2},{})
     for(var p in m){
         document.write(p);
     }
-
-   document.write((function(x){
+    //匿名函数定义及执行
+   document.write( (function(x){
        return x*x*x;
-   })(2)); //display 8;
-    //闭包  实现 私有持久的变量
-    UniqueID = (function() {
-        var id = 0;
-        return function() { return id++; }
-    })();
+   })(2) + ""); //display 8;
 
-    document.write(UniqueID());
+}
+
+//1.使用闭包的私有属性
+function userprivatevariable(){
     var o = {};
-    makeProperties(o,"Name",function(x){return x == "String";})
+    makeProperties(o,"Name",function(x){return typeof x == "string";})
     o.setName("Frank");
-    document.write(o.getName());
+    document.write(o.getName() + "<br>");
+    var arr = [makeFunc(0),makeFunc(1),makeFunc(2)];
+    document.write(arr[0] + "<br>");
+    document.write(arr[1] + "<br>");
+    document.write(arr[2] + "<br>");
+    //arr[0],arr[1],arr[2] 三个数组元素均返回 function (){return x;}
+    document.write(arr[0]() + "<br>");
+    document.write(arr[1]() + "<br>");
+    document.write(arr[2]() + "<br>");
+    //**但是三个执行返回的值分别是 0,1,2 说明函数是在它所定义的作用域中执行的**
+
+
 }
 
-function copyProperties(from,to){
-    if(!to) to = {};
-    for(var name in from){
-        to[name] = from[name];
-    }
-    return to;
+//返回函数
+function makeFunc(x){
+    return function(){return x;}
 }
-function makeProperties(o,name,predicate){
-    var value;
-    o["get"+name] = function(){return value};
-    o["set" + name] = function(v){
-        if(predicate && !predicate(v)){
-            throw "set" + name + ": invalid value" + value;
-        }else{
-            value = v;
-        }
+
+//测试函数的callee
+function testFuncCallee(){
+    var a = "hahaha";
+    var b = 10;
+    var c = function(){return 5};
+    testCallee(a,b,c);
+}
+
+function testCallee(a,b,c,d){
+    document.write(arguments.length + "<br>");//display 3;
+    document.write(arguments.callee.length + "<br>");//display 4;
+    document.write(arguments.callee + "<br>");//display 整个该函数
+}
+
+//使用闭包的断点
+function inspect(inspector,title){
+    var expression,result;
+    if("ignore" in arguments.callee) return;
+    while (true){
+        var message = "";
+        if(title) message = title + "\n";
+        if(expression) message += "\n" + expression + "==>" + result + "\n";
+        else expression = "";
+        message += "Enter an expression to evaluate:";
+        expression = prompt(message,expression);
+        if(!expression) return;
+        result = inspector(expression);
     }
 }
+
+function factorial(n){
+    var inspector = function($){ return eval($); }
+    inspect(inspector,"Entering factorial()");
+    var result = 1;
+    while (n>1){
+        result = result * n;
+        n--;
+        inspect(inspector,"Exiting factorial()");
+        return result;
+    }
+}
+
+//Function()构造函数,它所创建的函数并不使用词法作用域，相反，他们总是当作顶层的函数一样来编译
+function createFunction(){
+    var f = new Function("x","y","return x*y");
+    document.write(constructFunction()());//display glabal not local
+}
+var y = "global";
+function constructFunction(){
+    var y = "local";
+    return new Function("return y");
+}
+
+(function(name){
+    alert(document.getElementById('btn'));
+})("wanghj");
+
+alert(document.getElementById('btn'));
+document.read(function(){
+
+});
+
+
--- jspro/js/test.js
+++ jspro/js/test.js
@@ -1,108 +1,139 @@
 /**
- * Created by xf on 2015/11/23.
+ * Created by whj on 2015/11/23.
+ * 该文件主要用于测试
  */
 
-function init(){
-    var tempint = 2/'a'; // return NaN ; if(false)
-    tempint = 2/0; // return Infinity ; if(true)
-    tempint = 2/'0.5'; // return 4 ; if(true);
-    var tempstring = ""; // if(false)
-    var tempobject = null; // if(false)
-    var tempobj = undefined;	//if(false)
-
-    /*
-     if(isNaN(tempint)){
-     alert("isNaN");
-     }else{
-     alert("is not NaN");
-     }
-     if(isFinite(tempint)){
-     alert("is not infinity");
-     }else{//Infinity or NaN return true;
-     alert("is infinity");
-     }
-
-     if(tempobj){
-     alert("true");
-     }else{
-     alert("false");
-     }
-     */
-    var str='you\'re right,it can\'t be quote';
-
-    var index2 = str.indexOf("y");//begin from 0
-
-    var strsub = str.substring(0,6);//contains before and notcontains after ;return you're
-
-    var c = str.charAt(0); //return y
-
-
-    //函数
-
-    var point = {x:2,y:2};
-    var rectangle = {upperleft:{x:3,y:3},lowerright:{x:4,y:4}}
-    //alert(rectangle.upperleft.x);
-    //alert(rectangle.lowerright.y);
-
-    var o = 1+null; //return 1;
-    var st = "is " + null; //return is null
-
-    var my = {param:1,x:2}
-    // alert(my.prop == null); //return true;
-    var my = {param:1,x:2,prop:null}
-    //alert(my.prop == null); //return true;
-
-    //	alert(x.y == null);// error :x is not defined;
-
-    var dd;
-    //alert(dd); //return undefined;
-
-    var n = 2+undefined;//return NaN;
-
-    var s = "haha"+my.toString();//return haha[object,object]
-    var n1 = 5 + my.toString(); //return 5[object,object]
-    //alert(n1);
-    var num = 0;
-    testValue(num);
-    //alert(num);return 0
-    var date = new Date(2015,11,20);
-    //alert(date.format());
-    setDate(date);
-    //alert(date);
-
-    //var scope = "global scope";
-    //checkscope(scope);
-    //alert(scope);
-    //f();
-    var x;
-    alert(x);//display undefined
-    alert(u);//cause an error
-    u="dd";
-    alert(u);
+//测试各种返回值
+/*
+    if(NaN) is false
+    if(Infinity) is true
+    if("") is false
+    if(null) is false
+    if(undefined) is false;
+*/
+function testIf(){
+    var temp1 = 2/'a';
+    var temp2 = 2/0;
+    var temp3 = 2/'0.5';
+    var temp4 = "";
+    var temp5 = null;
+    var temp6 = undefined;
+   // document.write(temp1+"");// result is NaN ;
+   // document.write(temp2+"");// result is Infinity ;
+   // document.write(temp3+"");// result is 4 ;
+    if(temp1){} //if(false)
+    if(temp2){} //if(true)
+    if(temp4){} //if(false)
+    if(temp5){} //if(false)
+    if(temp6){} //if(false)
+    var i = 0123;//display 108
+        i = 123; //display 148
+    var m = i+25;
+    document.write(m + "");
 }
 
-//传值or传址
+//判断的是否是NaN
+function testIfNaN(v){
+   // var v = 5/'a';
+    if(isNaN(v)){
+        document.write("isNaN");
+    }else{
+        document.write("is not NaN");
+    }
+}
+
+//判断是否是无穷大
+function testIfInfinity(v){
+    // finite 有限
+    // infinite 无限
+    // infinity 无穷
+    // finity  有限
+    // v = 2/0 ;infinity
+    // m = 2/1 ;finity
+    if(!isFinite(v)){
+        document.write("is infinity");
+    }else{
+        document.write("is finity");
+    }
+}
+
+//测试字符串
+function testString(){
+    var str='you\'re right,it can\'t be quote';
+    var index2 = str.indexOf("y");//从0开始
+    str = str.substring(0,6);//包前不包后
+    var c = str.charAt(0); //return y ; 获取单个字符
+}
+
+//测试对象
+function testObject(){
+    var point = {x:2,y:2};
+    var rectangle = {left:{x:3,y:3},right:{x:4,y:4}}
+    document.write(point.x+"");
+    document.write(rectangle.left.x+"");
+    document.write(rectangle.right.y+"");
+}
+
+//测试加号用法
+function testAdd(){
+    var o = 1+null; //return 1;
+    var str = "is " + null; //return is null
+    var n = 2+undefined;//return NaN;
+    var my = {param:1,x:2}
+    var s = "haha"+my.toString();//return haha[object,object]
+    var m = 5 + my.toString(); //return 5[object,object]
+    document.write(o + "");
+    document.write(str + "");
+    document.write(n + "");
+    document.write(s + "");
+    document.write(m + "");
+}
+
+//测试对象属性为空
+function testObjAttr(){
+    var a = {param:1,x:2};
+    document.write(a.prop); //display undefined
+    document.write((a.prop == null) + ""); //display true
+    document.write((a.prop === null) + ""); //display false
+    var my = {param:1,x:2,prop:null}
+    document.write((my.prop === null) + ""); //return true;
+    //document.write(x.y == null);// error :x is not defined;
+}
+
+//测试函数传值 传值or传址
+function testFuncParam(){
+    var num = 0;
+    testValue(num);
+    document.write(num + ""); //display 0
+    var date = new Date(2015,11,20);
+    document.write(date + "<br>");
+    date.setDate(21);
+    document.write(date + "<br>");
+    var scope = "global scope";
+    checkScope(scope);
+    document.writeln(scope + "<br>");
+    f();
+}
 
 function testValue(m){
     m = m+1;
-    //alert(m);//return 1;
-}
-//传址
-function setDate(d){
-    d.setDate(21);
+   document.write(m + "");//display 1;
 }
 
-function checkscope(scope){
+function checkScope(scope){
     var scope = "local scope";
     function nested(){
         var scope = "nest scope";
-        alert(scope);
+        document.writeln(scope + "<br>");
     }
     nested();
+    document.writeln(scope + "<br>");
 }
+
+//注意：当局部变量和全局变量同名时,在函数里即使先使用未定义的局部变量,也不会赋予全部变量的值，而是undefined
 var scope="global";
 function f(){
-    alert(scope);
-    var scope2="local";
-    alert(scope2);
+    document.write(scope + "<br>");
+    var scope="local";
+    document.write(scope + "<br>");
 }
